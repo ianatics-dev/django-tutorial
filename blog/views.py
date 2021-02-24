2dd2683d5e6e0df30f3c3654cfd7db34342da9de
Ian Autentico <ianautentico598@gmail.com>
2021-02-24 22:57:38 +0800

    wasak

--- blog/views.py
+++ blog/views.py
@@ -1,9 +1,30 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 
+posts = [
+    {
+        'author': 'Ian Autentico',
+        'title': 'Blog Post 1',
+        'content': 'First post content',
+        'date_posted': 'Febuary 24, 2021',
+    },
+    {
+        'author': 'Angelica',
+        'title': 'Blog Post 2',
+        'content': 'Second post content',
+        'date_posted': 'Febuary 24, 2021',
+    },
+]
+
+
+
 def home(request):
-    return render(request, 'blog/home.html')
+    context = {
+        'posts' : posts
+    }
+    return render(request, 'blog/home.html', context)
 
 def about(request):
-    return HttpResponse('<h1>Blog About</h1>')
+
+    return render(request, 'blog/about.html',{'title':'About'})
 
